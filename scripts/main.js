0d60095eba82c8ceacafe7f8ad84f6b6b9c57db9
Nikos Kougianos <kougi_jr@hotmail.com>
2022-07-12 15:22:06 +0300

    REDESIGN

--- scripts/main.js
+++ scripts/main.js
@@ -1,14 +1,14 @@
+// @ts-nocheck
 // Add your javascript here
 // Don't forget to add it into respective layouts where this js file is needed
 
 $(document).ready(function () {
 	AOS.init({
 		// uncomment below for on-scroll animations to played only once
-		// once: true
+		once: true
 	}); // initialize animate on scroll library
 
 	// Scroll to top button
-
 	var btn = $('#buttonScrollTop');
 
 	if ($(window).scrollTop() > 300) {
